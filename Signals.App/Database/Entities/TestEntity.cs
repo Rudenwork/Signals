a74ca247042cd0360c2c49d6deb9c4d07bf8256e
Rudenwork <rudenwork@gmail.com>
2022-11-06 21:28:42 +0100

    Remove Test entity, model and controller

--- Signals.App/Database/Entities/TestEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Signals.App.Database.Entities
-{
-    public class TestEntity
-    {
-        public Guid Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
-    }
-}
--- Signals.App/Database/SignalsContext.cs
+++ Signals.App/Database/SignalsContext.cs
@@ -5,7 +5,6 @@ namespace Signals.App.Database
 {
     public class SignalsContext : DbContext
     {
-        public DbSet<TestEntity> Tests { get; set; }
         public DbSet<UserEntity> Users { get; set; }
 
         public SignalsContext(DbContextOptions<SignalsContext> options) : base(options) { }
