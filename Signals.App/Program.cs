4b580d0e8835006b90cc30e9256a5b86441efab8
Rudenwork <rudenwork@gmail.com>
2022-11-11 00:08:49 +0100

    Extract authority to configuration

--- Signals.App/Program.cs
+++ Signals.App/Program.cs
@@ -8,8 +8,11 @@ using Signals.App.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Signals.App.Database.Extentions;
 using Microsoft.OpenApi.Models;
+using Signals.App.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var settings = builder.Configuration.Get<Settings>();
 
 builder.Services.AddDbContext<SignalsContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString(nameof(Signals))));
@@ -42,11 +45,6 @@ builder.Services.AddIdentityServer()
     .AddAspNetIdentity<UserEntity>()
     .AddProfileService<ProfileService>();
 
-var authority = builder.Configuration["ASPNETCORE_URLS"]
-    .Split(";")
-    .First(u => u.Contains("https"))
-    .Replace("*", "localhost");
-
 builder.Services.AddAuthentication(options =>
     {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -54,7 +52,7 @@ builder.Services.AddAuthentication(options =>
     })
     .AddJwtBearer(options =>
     {
-        options.Authority = authority;
+        options.Authority = settings.Identity.Authority;
         options.TokenValidationParameters.ValidateAudience = false;
     });
 
@@ -67,7 +65,7 @@ builder.Services.AddSwaggerGen(options =>
     options.AddSecurityDefinition(nameof(SecuritySchemeType.OpenIdConnect), new OpenApiSecurityScheme
     {
         Type = SecuritySchemeType.OpenIdConnect,
-        OpenIdConnectUrl = new Uri($"{authority}/.well-known/openid-configuration")
+        OpenIdConnectUrl = new Uri($"{settings.Identity.Authority}/.well-known/openid-configuration")
     });
 
     options.AddSecurityRequirement(new OpenApiSecurityRequirement()
---
+++ Signals.App/Settings/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace Signals.App.Settings
+{
+    public class Settings
+    {
+        public IdentitySettings Identity { get; set; }
+
+        public class IdentitySettings
+        {
+            public string Authority { get; set; }
+        }
+    }
+}
