1ad17900ba16345096a259832dae4bb74a223ae8
Rudenwork <rudenwork@gmail.com>
2022-11-15 22:27:59 +0100

    Extract UserModel properties validation regex and error message to constants

--- Signals.App/Controllers/Models/UserModel.cs
+++ Signals.App/Controllers/Models/UserModel.cs
@@ -7,13 +7,13 @@ namespace Signals.App.Controllers.Models
         public class Create
         {
             [Required]
-            [RegularExpression(@"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{1,50}$",
-                ErrorMessage = $"May only contain Roman letters (a-z, A-Z), digits (0-9), and some special characters (._). Max length 50 symbols. Cannot begin with a period.")]
+            [RegularExpression(Constants.Username.Regex, ErrorMessage = Constants.Username.ErrorMessage)]
             public string Username { get; set; }
+
             [Required]
-            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-                ErrorMessage = $"Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
+            [RegularExpression(Constants.Password.Regex, ErrorMessage = Constants.Password.ErrorMessage)]
             public string Password { get; set; }
+
             public bool? IsAdmin { get; set; }
             public bool? IsDisabled { get; set; }
         }
@@ -28,14 +28,29 @@ namespace Signals.App.Controllers.Models
 
         public class Update
         {
-            [RegularExpression(@"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{1,50}$",
-                ErrorMessage = $"May only contain Roman letters (a-z, A-Z), digits (0-9), and some special characters (._). Max length 50 symbols. Cannot begin with a period.")]
+            [RegularExpression(Constants.Username.Regex, ErrorMessage = Constants.Username.ErrorMessage)]
             public string? Username { get; set; }
-            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
-                 ErrorMessage = $"Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
+
+            [RegularExpression(Constants.Password.Regex, ErrorMessage = Constants.Password.ErrorMessage)]
             public string? Password { get; set; }
+
             public bool? IsAdmin { get; set; }
             public bool? IsDisabled { get; set; }
+        }
+
+        private static class Constants
+        {
+            public static class Username
+            {
+                public const string Regex = @"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{1,50}$";
+                public const string ErrorMessage = "May only contain Roman letters (a-z, A-Z), digits (0-9), and some special characters (._). Max length 50 symbols. Cannot begin with a period.";
+            }
+
+            public static class Password
+            {
+                public const string Regex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+                public const string ErrorMessage = "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character";
+            }
         }
     }
 }
