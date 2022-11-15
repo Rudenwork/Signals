bf37e67de4542ca36d0d50f6f109c1e8837dbb7b
Rudenwork <rudenwork@gmail.com>
2022-11-15 20:42:51 +0100

    UserModel validation added

--- Signals.App/Controllers/Models/UserModel.cs
+++ Signals.App/Controllers/Models/UserModel.cs
@@ -7,8 +7,12 @@ namespace Signals.App.Controllers.Models
         public class Create
         {
             [Required]
+            [RegularExpression(@"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{1,50}$",
+                ErrorMessage = $"May only contain Roman letters (a-z, A-Z), digits (0-9), and some special characters (._). Max length 50 symbols. Cannot begin with a period.")]
             public string Username { get; set; }
             [Required]
+            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+                ErrorMessage = $"Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
             public string Password { get; set; }
             public bool? IsAdmin { get; set; }
             public bool? IsDisabled { get; set; }
@@ -24,7 +28,11 @@ namespace Signals.App.Controllers.Models
 
         public class Update
         {
+            [RegularExpression(@"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{1,50}$",
+                ErrorMessage = $"May only contain Roman letters (a-z, A-Z), digits (0-9), and some special characters (._). Max length 50 symbols. Cannot begin with a period.")]
             public string? Username { get; set; }
+            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+                 ErrorMessage = $"Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
             public string? Password { get; set; }
             public bool? IsAdmin { get; set; }
             public bool? IsDisabled { get; set; }
