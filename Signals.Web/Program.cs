59f2c8a77fdf69c2166c2f6902d42beb7f65e689
Rudenwork <rudenwork@gmail.com>
2023-02-25 13:38:51 +0100

    Add Signals.Web AppSettings and corresponding endpoint

--- Signals.Web/Program.cs
+++ Signals.Web/Program.cs
@@ -1,8 +1,12 @@
+using Signals.Web.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+app.Map("settings", () => app.Configuration.Get<AppSettings>());
 
 app.Run();
---
+++ Signals.Web/Settings/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Signals.Web.Settings
+{
+    public class AppSettings
+    {
+        public string ApiBaseAddress { get; set; }
+    }
+}
