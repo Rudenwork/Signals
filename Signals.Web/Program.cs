f3c2fed325f1fa5fa229155202174d6428ed04ca
Rudenwork <rudenwork@gmail.com>
2023-03-02 21:53:06 +0100

    Reverse proxy added to SIgnals.Web

--- Signals.Web/Program.cs
+++ Signals.Web/Program.cs
@@ -1,9 +1,17 @@
 using Signals.Web.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var proxySection = builder.Configuration.GetSection("ReverseProxy");
+proxySection["Clusters:default:Destinations:app:Address"] = builder.Configuration["ApiBaseAddress"];
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(proxySection);
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.MapReverseProxy();
 
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
