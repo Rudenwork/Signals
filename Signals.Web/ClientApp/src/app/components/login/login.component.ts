436c6aa5187fca698129b66d500b8f1edb7b1156
Rudenwork <rudenwork@gmail.com>
2023-03-20 19:15:24 +0100

    Add login validation

--- Signals.Web/ClientApp/src/app/components/login/login.component.ts
+++ Signals.Web/ClientApp/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -10,17 +11,27 @@ import { AuthService } from 'src/app/services/auth.service';
 export class LoginComponent implements OnInit {
     constructor(private authService: AuthService, private router: Router) { }
 
-    username: string = 'admin';
-    password: string = 'admin';
+    username!: FormControl;
+    password!: FormControl;
+    form!: FormGroup;
 
     ngOnInit() {
         if (this.authService.isAuthenticated) {
             this.router.navigate(['/']);
         }
+
+        this.username = new FormControl('', Validators.required);
+        this.password = new FormControl('', Validators.required);
+
+        this.form = new FormGroup([
+            this.username,
+            this.password
+        ]);
     }
 
     login() {
-        this.authService.login(this.username, this.password)
-            .then(() => this.router.navigate(['/']));
+        this.authService.login(this.username.value, this.password.value)
+            .then(() => this.router.navigate(['/']))
+            .catch(() => this.form.reset());
     }
 }
