0af557faeb6a0e09dfa2876a730bd7a2ac92db67
Vadym Rudenko <114187965+Rudenwork@users.noreply.github.com>
2023-04-11 12:22:56 +0200

    Fix channel verification code property type

--- Signals.Web/ClientApp/src/app/components/channels/channel-form-verify/channel-form-verify.component.ts
+++ Signals.Web/ClientApp/src/app/components/channels/channel-form-verify/channel-form-verify.component.ts
@@ -37,6 +37,6 @@ export class ChannelFormVerifyComponent implements OnInit {
             return;
         }
 
-        this.submitted.emit(this.code.value);
+        this.submitted.emit(this.code.value.toString());
     }
 }
