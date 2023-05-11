336d0e010d56923cd0f4a4ee9135a264e8839252
Rudenwork <rudenwork@gmail.com>
2023-05-11 20:43:45 +0200

    Home page separated

--- Signals.Web/ClientApp/src/app/app-routing.module.ts
+++ Signals.Web/ClientApp/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChannelsPageComponent } from './components/channels/channels-page/channels-page.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomePageComponent } from './components/home/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { SignalsComponent } from './components/signals/signals.component';
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: '', component: HomePageComponent, pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'logout', component: LogoutComponent },
     { path: 'users', component: UsersComponent },
--- Signals.Web/ClientApp/src/app/app.module.ts
+++ Signals.Web/ClientApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomePageComponent } from './components/home/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -60,7 +60,7 @@ import { SmaPreviewComponent } from './components/signals/signal-form/stage-form
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent,
+        HomePageComponent,
         LoginComponent,
         LogoutComponent,
         MenuComponent,
