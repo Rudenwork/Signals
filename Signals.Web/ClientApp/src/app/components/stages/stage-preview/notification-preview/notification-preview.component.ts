b64f263ad26d6c1231805548a1212e53b435d95f
Rudenwork <rudenwork@gmail.com>
2023-05-20 22:01:05 +0200

    Introduce Waiting and Condition stage previews

--- Signals.Web/ClientApp/src/app/components/stages/stage-preview/notification-preview/notification-preview.component.ts
+++ Signals.Web/ClientApp/src/app/components/stages/stage-preview/notification-preview/notification-preview.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Channel } from 'src/app/models/channel.model';
 import { NotificationStage } from 'src/app/models/signal.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
     selector: 'app-notification-preview',
     templateUrl: './notification-preview.component.html',
     styleUrls: ['./notification-preview.component.scss']
 })
-export class NotificationPreviewComponent {
+export class NotificationPreviewComponent implements OnInit {
+    constructor(private dataService: DataService) { }
+
     @Input() stage!: NotificationStage;
+    channel!: Channel;
+
+    ngOnInit() {
+        this.dataService.getChannel(this.stage.channelId ?? '')
+            .subscribe(channel => {
+                this.channel = channel;
+            });
+    }
 }
